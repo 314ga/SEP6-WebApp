8a6f35c6b4a473109ed06066f1dfd6cdf05b48a4
314ga <jakub.piga.cp@gmail.com>
2020-12-05 19:45:39 +0100

    Added tables and toogle buttons logic

--- sep6-web-app/src/components/Weather.js
+++ sep6-web-app/src/components/Weather.js
@@ -1,13 +1,126 @@
 import React from "react";
-import { Link } from "react-router-dom";
 import AppNavbar from '../components/AppNavbar'
+import MUIDataTable from "mui-datatables";
+import { makeStyles, withStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import ToggleButton from '@material-ui/lab/ToggleButton';
+import ToggleButtonGroup from '@material-ui/lab/ToggleButtonGroup';
+import store from '../store';
+import {useSelector} from 'react-redux'
+import { retrieveWeatherData } from '../reducers/weatherData';
+const useStyles = makeStyles((theme) => ({
+    paper: {
+      display: 'flex',
+      textAlign: 'center',
+      border: `1px solid ${theme.palette.divider}`,
+      flexWrap: 'wrap',
+    },
+    divider: {
+      margin: theme.spacing(1, 0.5),
+    },
+  }));
+  
+  const StyledToggleButtonGroup = withStyles((theme) => ({
+    grouped: {
+      margin: theme.spacing(0.5),
+      border: 'none',
+    background: 'linear-gradient(240deg, #abe9cd 0%, #3eadcf 74%)',
+    boxShadow: '0 3px 5px 2px #BDD4E7',
+      '&:not(:first-child)': {
+        borderRadius: theme.shape.borderRadius,
+      },
+      '&:first-child': {
+        borderRadius: theme.shape.borderRadius,
+      },
+    },
+  }))(ToggleButtonGroup);
 
-const Weather = () => {
+const Weather = () => 
+{
+    const columns = [
+        {
+         name: "origin",
+         label: "Origin",
+         options: {
+          filter: true,
+          sort: true,
+         }
+        },
+        {
+         name: "temp",
+         label: "Temperature",
+         options: {
+          filter: true,
+          sort: false,
+         }
+        },
+        {
+         name: "weather_obs_origin",
+         label: "Observations for origin",
+         options: {
+          filter: true,
+          sort: false,
+         }
+        },
+       ];
+       
+     
+       
+       const options = {
+         filterType: 'checkbox',
+       };
+       //weatherData.map(weatherData => <div>{weatherData.key}</div>)
+       const weatherData = useSelector(state =>  state.weatherData);
+       const data = weatherData;
+       /* TOOGLE**/
+       const [alignment, setAlignment] = React.useState('left');
+
+        const handleDataChange = (event, dataChange) => 
+        {
+            store.dispatch(retrieveWeatherData(dataChange));
+        };
+
+        const classes = useStyles();
+      
     return (
 
         <div>
             <AppNavbar />
-            <p>Weather page</p>
+
+
+            <Paper elevation={0} className={classes.paper}>
+        <StyledToggleButtonGroup
+          size="small"
+          value={alignment}
+          exclusive
+          onChange={handleDataChange}
+          aria-label="text alignment"
+        >
+          <ToggleButton value="wo-origins" aria-label="left aligned">
+            Weather observation for origin
+          </ToggleButton>
+          <ToggleButton value="temp-attributes" aria-label="centered">
+            Origin temperatures
+          </ToggleButton>
+          <ToggleButton value="temp-jfk" aria-label="right aligned">
+            JFK temperature
+          </ToggleButton>
+          <ToggleButton value="avgtemp-jfk" aria-label="justified">
+            Mean temperature JFK
+          </ToggleButton>
+          <ToggleButton value="avgtemp-origin" aria-label="justified">
+            Mean temperature origins
+          </ToggleButton>
+        </StyledToggleButtonGroup>
+      </Paper>
+
+
+            <MUIDataTable
+            title={"Employee List"}
+            data={data}
+            columns={columns}
+            options={options}
+            />
         </div>
     );
 };
